aef3bad453d59492148d113f6a9c7189bd86ee0d
Skintrphoenix <randy.phoenix.098@gmail.com>
2023-02-16 14:33:31 +0700

    update resource

--- src/Testing/TestCon.php
+++ src/Testing/TestCon.php
@@ -7,10 +7,14 @@ use Skintrphoenix\PluginLoader\Controller\Controller;
 
 class TestCon extends Controller{
 
+    public function __construct()
+    {
+        parent::__construct('ok/{args1}', 'ok',Testing::getInstance());
+    }
+
     public function controller(Request $request, array $args)
     {
         if(!isset($args[0])){
-
             return 'def';
         }else{
             switch($args[0]){
--- src/Testing/Testing.php
+++ src/Testing/Testing.php
@@ -6,8 +6,15 @@ use Skintrphoenix\PluginLoader\Plugin\PluginBase;
 
 class Testing extends PluginBase{
 
+    private static $instance;
+
     public function onLoad():void{
-        $cmd = new TestCon('testing/{args1}', $this);
+        self::$instance = $this;
+        $cmd = new TestCon();
         $this->register_route($cmd);
+    }
+
+    public static function getInstance() : Testing{
+        return self::$instance;
     }
 }
